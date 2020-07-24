6d9adab77ec6b217649f0791e9d23a270675ff51
Elikworld <koladeodutoye9913@gmail.com>
2020-07-24 09:39:08 +0100

    index.js modified

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	res.write("Hello World, welcome to WeJapa Internships");
-	res.end();
+	if (req.method === "GET" && req.url === "/") {
+		res.writeHead("200", { "Content-Type": "text/plain" });
+		res.write("Hello World, welcome to WeJapa Internships");
+		res.end();
+	} else {
+		res.writeHead("404", { "Content-Type": "text/plain" });
+		res.write("Page not found");
+		res.end();
+	}
 });
 
 const port = 3000;
